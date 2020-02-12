372626f9b1abe43d4dff52831fb361fb30e79eb1
mwilliamsmusic <fasmusic@gmail.com>
2020-02-12 15:50:56 -0500

    Story 3

--- src/AudioList/AudioList.js
+++ src/AudioList/AudioList.js
@@ -9,6 +9,7 @@ function AudioList(props) {
     let info;
 
     if (!toggle) {
+
         info = <table>
             <tr>
                 <td>{props.label} </td>
@@ -17,28 +18,51 @@ function AudioList(props) {
                 <td>
                     <button onClick={() => setToggle(!toggle)}>Track Info</button>
                 </td>
-
-                <td><audio controls={true} src={props.url}></audio>
+                <td>
+                    <div style={error ? {display: 'none'} : null}>
+                        <audio onError={() => setError(true)}
+                               controls={true} src={props.url}></audio>
+                    </div>
+                    <div style={error ? {display: 'table-cell'} : {display: 'none'}}>
+                        <div className={styles.errorDisplay}> <div className={styles.boldText}>File Not Found</div></div>
+                    </div>
                 </td>
             </tr>
         </table>
 
     } else {
+
         info = <div className={styles.trackInfo}>
             <ul>
-                <li>{props.label}</li>
-                <li>{props.speaker}</li>
-                <li>{props.category}</li>
-                <li>{props.description}</li>
-
                 <li>
-
+                    <div className={styles.boldText}> Title:</div>
+                    {props.label}
+                </li>
+                <li>
+                    <div className={styles.boldText}> Speaker:</div>
+                    {props.speaker}
+                </li>
+                <li>
+                    <div className={styles.boldText}> Category:</div>
+                    {props.category}
+                </li>
+                <li>
+                    <div className={styles.boldText}>Description:</div>
+                    {props.description}
+                </li>
+                <li>
+                    <div style={error ? {display: 'none'} : null}>
                         <audio onError={() => setError(true)}
                                controls={true} src={props.url}></audio>
+                    </div>
                 </li>
-
                 <li>
-                    <button onClick={() => setToggle(!toggle)}>Close</button>
+                    <div style={error ? {display: 'table-cell'} : {display: 'none'}}>
+                        <div className={styles.errorDisplay}></div>
+                    </div>
+                </li>
+                <li>
+                    <button  onClick={() => setToggle(!toggle)}>Close</button>
                 </li>
             </ul>
         </div>
@@ -49,8 +73,6 @@ function AudioList(props) {
             {info}
         </div>
     )
-
-
 }
 
 export default AudioList;
--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,57 +1,55 @@
 import React from "react";
-import audioData from '../AudioFiles/audio_files (1).json'
+import audioData from '../AudioData/audio_files (1)'
 import AudioList from "../AudioList/AudioList";
 import styles from "./Display.module.css"
+
 class Display extends React.Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             category: 'Meditation',
         }
     }
-
 
     render() {
 
         const categorySelect = audioData.filter((data) => {
             return data.category === this.state.category
         });
-
         const meditation = () => {
             this.setState({category: "Meditation"})
         };
-
         const breathing = () => {
             this.setState({category: "Breathing"})
         };
-
         const relaxation = () => {
             this.setState({category: "Relaxation"})
         };
-const listItems = categorySelect.map((data)=>
+        const listItems = categorySelect.map((data) =>
+            <AudioList key={data.id}
+                       label={data.label}
+                       category={data.category}
+                       speaker={data.speaker}
+                       url={data.url}
+                       description={data.description}/>
+        );
 
-        <AudioList key={data.id}
-                   label={data.label}
-                   category={data.category}
-                   speaker={data.speaker}
-                   url={data.url}
-                   description={data.description} />
-
-    );
         return (
 
             <div className={styles.gridContainer}>
                 <div className={styles.category}>
-                <p>Please select a category: </p>
-                <button onClick={meditation}>Meditation</button>
-                <button onClick={breathing}>Breathing</button>
-                <button onClick={relaxation}>Relaxation</button>
+                    <p>Please Select A Category </p>
+                    <button onClick={meditation}>Meditation</button>
+                    <button onClick={breathing}>Breathing</button>
+                    <button onClick={relaxation}>Relaxation</button>
                 </div>
 
-            <div className={styles.tableContainer}>
-                {listItems}
-            </div>
+                <div className={styles.tableContainer}>
+                    {listItems}
+                </div>
+
             </div>
         );
     }
